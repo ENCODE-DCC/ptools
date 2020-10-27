850de3c5cb8007b36394a5dafcbaf6f8c6d9c420
ottojolanki <ojolanki@stanford.edu>
2020-10-27 08:41:39 -0700

    fix typo in 10x makeFastq.sh

--- 10xscell/pfastq/makeFastq.sh
+++ 10xscell/pfastq/makeFastq.sh
@@ -11,4 +11,4 @@ samtools view "${pbam_path}" | python3 "$(which make_unique.py)" > reads.txt
 awk '!seen[$1$2$3$4]++' reads.txt > unique_reads.txt
 awk '{print $5}' unique_reads.txt > linenumbers.txt
 samtools view "${pbam_path}" | python3 "$(which print_unique.py)" | samtools view -h -bS - > filtered.bam
-samtools view filtered.bam | python3 "$(python3 10x_bam2fastq.py)" "${pbam_prefix}"
+samtools view filtered.bam | python3 "$(which 10x_bam2fastq.py)" "${pbam_prefix}"
