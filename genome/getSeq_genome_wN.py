0fc66900e55586c510695380d8508e23e0215abf
Gamze Gursoy <gamze.gursoy@yale.edu>
2020-12-08 08:53:15 -0500

    fixed the chr naming conventions

--- genome/getSeq_genome_wN.py
+++ genome/getSeq_genome_wN.py
@@ -1,5 +1,5 @@
 ## Gamze Gursoy  ##
-## last edit: March 24th, 2020
+## last edit: Dec 7th, 2020
 ## input arguments
 ## (1) reference genome
 ## (2) diff folder
@@ -16,7 +16,8 @@ import csv
 import zlib, sys, time, base64
 import gzip
 import numpy as np
-import PrintSequence
+import io
+from . import PrintSequence
 
 
 def ParseCigar(cigar):
@@ -32,105 +33,109 @@ def ParseCigar(cigar):
 
 
 # following is necessary for querying sequences from reference genome
-with open(sys.argv[1], "rt") as f:
-    ref = PrintSequence.Lookup(f)
+def main():
 
-bam = []
-import io
+    conv1=['chr1','chr2','chr3','chr4','chr5','chr6','chr7','chr8','chr9','chr10','chr11','chr12','chr13','chr14','chr15','chr16','chr17','chr18','chr19','chr20','chr21','chr22','chrX','chrY','chrM','chrMT']
 
-hed = open(sys.argv[2], "r")
-header = []
-for line in hed:
-    header.append(line.split("\n")[0])
-hed.close()
+    conv2=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','X','Y','M','MT']
+    with open(sys.argv[1], "rt") as f:
+        ref = PrintSequence.Lookup(f)
 
-for i in range(0, len(header)):
-    print("%s" % header[i])
-
-fileB = open(sys.argv[3], "r")
-for lineB in fileB:
-    p = lineB.rstrip()
-    pbam = p.split("\t")
-    RL = len(pbam[9])
-    chrom = str(pbam[2])
-    if "chr" in chrom:
-        parsedCigar = ParseCigar(pbam[5])
-        for i in range(0, len(parsedCigar)):
-            if parsedCigar[i][1] == "N":
-                midpoint = i
-                break
-        lenbeforemid = 0
-        delLength = 0
-        for i in range(0, midpoint):
-            if parsedCigar[i][1] != "D":
-                lenbeforemid = lenbeforemid + int(parsedCigar[i][0])
-            if parsedCigar[i][1] == "D":
-                delLength = int(parsedCigar[i][0])
-        lenaftermid = RL - lenbeforemid
-        pbam[5] = (
-            str(lenbeforemid)
-            + "M"
-            + str(parsedCigar[midpoint][0])
-            + "N"
-            + str(lenaftermid)
-            + "M"
-        )
-        nColpbam = len(pbam)
-        for i in range(0, nColpbam):
-            t = pbam[i].split(":")
-            if t[0] == "MD":
-                pbam[i] = "MD:Z:" + str(RL)
-            if t[0] == "AS":
-                pbam[i] = "AS:i:" + str(RL)
-            if t[0] == "NM":
-                pbam[i] = "NM:i:0"
-            if t[0] == "nM":
-                pbam[i] = "nM:i:0"
-	    if t[0] == "MC":
-		pbam[i] = "MC:Z:" + str(RL) + "M"
-        if parsedCigar[0][1] == "S" or parsedCigar[0][1] == "D":
-            startPos = int(pbam[3]) - int(parsedCigar[0][0]) + delLength
-        else:
-            startPos = int(pbam[3]) + delLength
-        seqbefore = ref.query(chrom, startPos - 1, int(lenbeforemid))
-        seqafter = ref.query(
-            chrom,
-            startPos + lenbeforemid + parsedCigar[midpoint][0] - 1,
-            int(lenaftermid),
-        )
-        pbam[3] = startPos
-        pbam[9] = seqbefore + seqafter
-        if len(pbam[9]) < int(RL):
-            b = len(pbam[9])
-            a = int(RL) - len(pbam[9])
-            for i in range(0, a):
-                pbam[9] = pbam[9] + "N"
-                if b > lenbeforemid:
-                    pbam[5] = (
-                        str(lenbeforemid)
-                        + "M"
-                        + str(parsedCigar[midpoint][0])
-                        + "N"
-                        + str(b - lenbeforemid)
-                        + "M"
-                        + str(a)
-                        + "S"
-                    )
-                if b == lenbeforemid:
-                    pbam[5] = (
-                        str(lenbeforemid)
-                        + "M"
-                        + str(parsedCigar[midpoint][0])
-                        + "N"
-                        + str(a)
-                        + "S"
-                    )
-                if b < lenbeforemid:
-                    pbam[5] = str(b) + "M" + str(a) + "S"
-    nbam = str(pbam[0]) + "\t"
-    for i in range(1, nColpbam - 1):
-        nbam = nbam + str(pbam[i]) + "\t"
-    nbam = nbam + str(pbam[nColpbam - 1])
-    print(nbam)
-    nbam = ""
     bam = []
+
+    hed = open(sys.argv[2], "r")
+    header = []
+    for line in hed:
+        header.append(line.split("\n")[0])
+    hed.close()
+
+    for i in range(0, len(header)):
+        print("%s" % header[i])
+
+    fileB = open(sys.argv[3], "r")
+    for lineB in fileB:
+        p = lineB.rstrip()
+        pbam = p.split("\t")
+        RL = len(pbam[9])
+        chrom = str(pbam[2])
+        if (chrom in conv1) or (chrom in conv2):
+            parsedCigar = ParseCigar(pbam[5])
+            for i in range(0, len(parsedCigar)):
+                if parsedCigar[i][1] == "N":
+                    midpoint = i
+                    break
+            lenbeforemid = 0
+            delLength = 0
+            for i in range(0, midpoint):
+                if parsedCigar[i][1] != "D":
+                    lenbeforemid = lenbeforemid + int(parsedCigar[i][0])
+                if parsedCigar[i][1] == "D":
+                    delLength = int(parsedCigar[i][0])
+            lenaftermid = RL - lenbeforemid
+            pbam[5] = (
+                str(lenbeforemid)
+                + "M"
+                + str(parsedCigar[midpoint][0])
+                + "N"
+                + str(lenaftermid)
+                + "M"
+            )
+            nColpbam = len(pbam)
+            for i in range(0, nColpbam):
+                t = pbam[i].split(":")
+                if t[0] == "MD":
+                    pbam[i] = "MD:Z:" + str(RL)
+                if t[0] == "AS":
+                    pbam[i] = "AS:i:" + str(RL)
+                if t[0] == "NM":
+                    pbam[i] = "NM:i:0"
+                if t[0] == "nM":
+                    pbam[i] = "nM:i:0"
+                if t[0] == "MC":
+                    pbam[i] = "MC:Z:" + str(RL) + "M"
+            if parsedCigar[0][1] == "S" or parsedCigar[0][1] == "D":
+                startPos = int(pbam[3]) - int(parsedCigar[0][0]) + delLength
+            else:
+                startPos = int(pbam[3]) + delLength
+            seqbefore = ref.query(chrom, startPos - 1, int(lenbeforemid))
+            seqafter = ref.query(
+                chrom,
+                startPos + lenbeforemid + parsedCigar[midpoint][0] - 1,
+                int(lenaftermid),
+            )
+            pbam[3] = startPos
+            pbam[9] = seqbefore + seqafter
+            if len(pbam[9]) < int(RL):
+                b = len(pbam[9])
+                a = int(RL) - len(pbam[9])
+                for i in range(0, a):
+                    pbam[9] = pbam[9] + "N"
+                    if b > lenbeforemid:
+                        pbam[5] = (
+                            str(lenbeforemid)
+                            + "M"
+                            + str(parsedCigar[midpoint][0])
+                            + "N"
+                            + str(b - lenbeforemid)
+                            + "M"
+                            + str(a)
+                            + "S"
+                        )
+                    if b == lenbeforemid:
+                        pbam[5] = (
+                            str(lenbeforemid)
+                            + "M"
+                            + str(parsedCigar[midpoint][0])
+                            + "N"
+                            + str(a)
+                            + "S"
+                        )
+                    if b < lenbeforemid:
+                        pbam[5] = str(b) + "M" + str(a) + "S"
+        nbam = str(pbam[0]) + "\t"
+        for i in range(1, nColpbam - 1):
+            nbam = nbam + str(pbam[i]) + "\t"
+        nbam = nbam + str(pbam[nColpbam - 1])
+        print(nbam)
+        nbam = ""
+        bam = []
--- genome/getSeq_genome_woN.py
+++ genome/getSeq_genome_woN.py
@@ -1,5 +1,5 @@
 ## Gamze Gursoy ##
-##last edit March 24th, 2020
+##last edit Dec 7th, 2020
 ## input arguments
 ## (1) reference genome
 ## (2) diff folder
@@ -16,59 +16,66 @@ import csv
 import zlib, sys, time, base64
 import gzip
 import numpy as np
-import PrintSequence
-
-# following is necessary for querying sequences from reference genome
-with open(sys.argv[1], "rt") as f:
-    ref = PrintSequence.Lookup(f)
-
-bam = []
 import io
+from . import PrintSequence
 
-hed = open(sys.argv[2], "r")
-header = []
-for line in hed:
-    header.append(line.split("\n")[0])
-hed.close()
 
-for i in range(0, len(header)):
-    print("%s" % header[i])
+def main():
 
-fileB = open(sys.argv[3], "r")
-for lineB in fileB:
-    p = lineB.rstrip()
-    pbam = p.split("\t")
-    RL = len(pbam[9])
-    nColpbam = len(pbam)
-    for i in range(0, nColpbam):
-        t = pbam[i].split(":")
-        if t[0] == "MD":
-            pbam[i] = "MD:Z:" + str(RL)
-        if t[0] == "AS":
-            pbam[i] = "AS:i:" + str(RL)
-        if t[0] == "NM":
-            pbam[i] = "NM:i:0"
-        if t[0] == "nM":
-            pbam[i] = "nM:i:0"
-	if t[0] == "MC":
-            pbam[i] = "MC:Z:" + str(RL) + "M"
-    chrom = str(pbam[2])
-    startPos = int(pbam[3])
-    if pbam[5] != "*" and "chr" in chrom:
-        pbam[9] = ref.query(chrom, startPos - 1, int(RL))
-        pbam[5] = str(RL) + "M"
-        if len(pbam[9]) < int(RL):
-            a = int(RL) - len(pbam[9])
-            pbam[5] = str(RL - a) + "M" + str(a) + "S"
-            for i in range(0, a):
-                pbam[9] = pbam[9] + "N"
-    else:
-        pbam[9] = pbam[9]
-        pbam[5] = pbam[5]
-    nbam = str(pbam[0]) + "\t"
-    for i in range(1, nColpbam - 1):
-        nbam = nbam + str(pbam[i]) + "\t"
-    nbam = nbam + str(pbam[nColpbam - 1])
-    print(nbam)
-    nbam = ""
+    conv1=['chr1','chr2','chr3','chr4','chr5','chr6','chr7','chr8','chr9','chr10','chr11','chr12','chr13','chr14','chr15','chr16','chr17','chr18','chr19','chr20','chr21','chr22','chrX','chrY','chrM','chrMT']
+
+    conv2=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','X','Y','M','MT']
+
+    # following is necessary for querying sequences from reference genome
+    with open(sys.argv[1], "rt") as f:
+        ref = PrintSequence.Lookup(f)
+
     bam = []
+
+    hed = open(sys.argv[2], "r")
+    header = []
+    for line in hed:
+        header.append(line.split("\n")[0])
+    hed.close()
+
+    for i in range(0, len(header)):
+        print("%s" % header[i])
+
+    fileB = open(sys.argv[3], "r")
+    for lineB in fileB:
+        p = lineB.rstrip()
+        pbam = p.split("\t")
+        RL = len(pbam[9])
+        nColpbam = len(pbam)
+        for i in range(0, nColpbam):
+            t = pbam[i].split(":")
+            if t[0] == "MD":
+                pbam[i] = "MD:Z:" + str(RL)
+            if t[0] == "AS":
+                pbam[i] = "AS:i:" + str(RL)
+            if t[0] == "NM":
+                pbam[i] = "NM:i:0"
+            if t[0] == "nM":
+                pbam[i] = "nM:i:0"
+            if t[0] == "MC":
+                pbam[i] = "MC:Z:" + str(RL) + "M"
+        chrom = str(pbam[2])
+        startPos = int(pbam[3])
+        if pbam[5] != "*" and (chrom in conv1 or chrom in conv2):
+            pbam[9] = ref.query(chrom, startPos - 1, int(RL))
+            pbam[5] = str(RL) + "M"
+            if len(pbam[9]) < int(RL):
+                a = int(RL) - len(pbam[9])
+                pbam[5] = str(RL - a) + "M" + str(a) + "S"
+                for i in range(0, a):
+                    pbam[9] = pbam[9] + "N"
+        else:
+            pbam[9] = pbam[9]
+            pbam[5] = pbam[5]
+        nbam = str(pbam[0]) + "\t"
+        for i in range(1, nColpbam - 1):
+            nbam = nbam + str(pbam[i]) + "\t"
+        nbam = nbam + str(pbam[nColpbam - 1])
+        print(nbam)
+        nbam = ""
+        bam = []
